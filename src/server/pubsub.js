c57152c90c47ce69c71008653dfc45a2b4f85600
luca.ronca <Cenerentola2>
2017-02-14 16:13:00 +0100

    publish refactored with es6 class and added publishSync method

--- src/server/pubsub.js
+++ src/server/pubsub.js
@@ -7,23 +7,17 @@ class Subscribe {
   constructor(topic, executor) {
     this.topic = topic;
     this.executor = executor;
-    // push the listener execution at the end of the callstack
-    // so the originator of topics will not be blocked while consumers process them.
-    setTimeout(_setExecutor.bind(this), 0);
-    
-    // private method
-    function _setExecutor() {
-      // Create the topic's object if not yet created
-      if(!_hasTopic(this.topic)) _topics[this.topic] = [];
 
-        if (typeof this.executor != 'function'){
-            console.error('Subscribe executor must be a function')
-            return;
-        }
-        // Add the listener to queue
-        this.index = _topics[this.topic].push(this.executor) -1;
+    // Create the topic's object if not yet created
+    if(!_hasTopic(this.topic)) _topics[this.topic] = [];
 
+    if (typeof this.executor != 'function'){
+        console.error('Subscribe executor must be a function')
+        return;
     }
+    // Add the listener to queue
+    this.index = _topics[this.topic].push(this.executor) -1;
+
   }
   
   remove() {
@@ -33,20 +27,50 @@ class Subscribe {
 
 }
 
-const PubSub = {
-
-  Subscribe,
-
-  publish: (topic, data) => {
-    // If the topic doesn't exist, or there's no listeners in queue, just leave
-    if(!_hasTopic(topic)) return;
-
-    // Cycle through _topics queue, fire!
-    _topics[topic].forEach((executor) => {
-      executor(data || {});
-    });
+class Publish {
+  constructor(topic, data, async) {
+    this.topic = topic;
+    this.data = data;
+    this.async = async;
   }
+  fire() {
+    // If the topic doesn't exist, or there's no listeners in queue, just leave
+    if(!_hasTopic(this.topic)) return;
 
+    this.topic = _checkValidTypes(this.topic);
+    if(!this.topic) return;
+
+    // trigger the executors
+    if (this.async) setTimeout(_fireExecutors.bind(this), 0);
+    else _fireExecutors.call(this);
+  }
+}
+
+function _fireExecutors() {
+    // Cycle through _topics queue, fire!
+    _topics[this.topic].forEach((executor) => {
+      executor(this.data || {});
+    });
+}
+
+function _checkValidTypes(entry) {
+    if (typeof entry != 'string' && typeof entry != 'number') {
+        console.error('topic parameter must be a valide type');
+        return false;
+    }
+    return String(entry);
+}
+
+const PubSub = {
+  Subscribe,
+  publish: (topic, data) => {
+    let pub = new Publish(topic, data, true);
+    return pub.fire();
+  },
+  publishSync: (topic, data) => {
+    let pub = new Publish(topic, data, false);
+    return pub.fire();
+  }
 }
 
 module.exports = PubSub;
